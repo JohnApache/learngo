e157e358188bd592cc70545ece804abf296a0be3
1468733552 <1468733552@qq.com>
2018-05-23 09:13:38 +0800

    x

--- eightQueen/eight_queen.go
+++ eightQueen/eight_queen.go
@@ -5,29 +5,22 @@ func main() {
 	//	使得任何一个皇后都无法直接吃掉其他的皇后？为了达到此目的，任两个皇后都不能处于同一条横行、
 	//	纵行或斜线上。八皇后问题可以推广为更一般的n皇后摆放问题：
 	//	这时棋盘的大小变为n1×n1，而皇后个数也变成n2。而且仅当 n2 = 1 或 n1 ≥ 4 时问题有解。
+	pb := NewPiecesBoard(8, 8)
+	for i := 0; i < len(pb.Board); i++ {
 
+	}
 }
 
-var (
-	maxX = 8
-	maxY = 8
-)
-
 type PiecesBoard struct {
-	Board []Piece
+	Board [][]Piece
+	maxX  int
+	maxY  int
 }
 
 type Piece struct {
 	X     int
 	Y     int
 	IsUse bool
-}
-
-func (p *Piece) HoldPieces() *[]Piece {
-	ps := []Piece{}
-	for i:=0;i<maxX
-	ps = append(ps, *p)
-	return nil
 }
 
 func NewPiece(x, y int) Piece {
@@ -39,21 +32,91 @@ func NewPiece(x, y int) Piece {
 }
 
 func NewPiecesBoard(mx, my int) *PiecesBoard {
-	pb := PiecesBoard{}
+	pb := PiecesBoard{
+		maxX: mx,
+		maxY: my,
+	}
 	for i := 0; i < mx; i++ {
+		HorPieces := []Piece{}
 		for j := 0; j < my; j++ {
-			pb.Board = append(pb.Board, NewPiece(i, j))
+			HorPieces = append(HorPieces, NewPiece(j, i))
 		}
+		pb.Board = append(pb.Board, HorPieces)
 	}
 	return &pb
 }
 
 func (pb *PiecesBoard) ClearUse() {
-	for _, v := range pb.Board {
-		v.IsUse = false
+	for i, _ := range pb.Board {
+		for j, _ := range pb.Board[i] {
+			pb.Board[i][j].IsUse = false
+		}
 	}
 }
 
-func (pb *PiecesBoard) HoldPiece() {
+//holdAll
+func (pb *PiecesBoard) HoldPiece(p *Piece) {
+	pb.holdHorPiece(p.Y)
+	pb.holdVerPiece(p.X)
+	pb.holdRsPiece(p.X, p.Y)
+	pb.holdLsPiece(p.X, p.Y)
+}
 
+//horizontal hold
+func (pb *PiecesBoard) holdHorPiece(y int) {
+	hp := pb.Board[y]
+	for i, _ := range hp {
+		pb.Board[y][i].IsUse = true
+	}
+}
+
+//vertical hold
+func (pb *PiecesBoard) holdVerPiece(x int) {
+	for _, v := range pb.Board {
+		v[x].IsUse = true
+	}
+}
+
+//right Slant
+func (pb *PiecesBoard) holdRsPiece(x, y int) {
+	tempX := x - 1
+	tempY := y - 1
+	for {
+		if x >= pb.maxX || y >= pb.maxY {
+			break
+		}
+		pb.Board[y][x].IsUse = true
+		x++
+		y++
+	}
+	for {
+		if tempX < 0 || tempY < 0 {
+			break
+		}
+		pb.Board[tempY][tempX].IsUse = true
+		tempX--
+		tempY--
+	}
+}
+
+//left Slant
+func (pb *PiecesBoard) holdLsPiece(x, y int) {
+	tempX := x - 1
+	tempY := y + 1
+	for {
+		if x >= pb.maxX || y < 0 {
+			break
+		}
+		pb.Board[y][x].IsUse = true
+		x++
+		y--
+	}
+	for {
+		if tempX < 0 || tempY >= pb.maxY {
+			break
+		}
+		pb.Board[tempY][tempX].IsUse = true
+		tempX--
+		tempY++
+	}
 }
--- eightQueen/eight_queen_test.go
+++ eightQueen/eight_queen_test.go
@@ -7,6 +7,14 @@ import (
 )
 
 func TestNewPiecesBoard(t *testing.T) {
-	pb := NewPiecesBoard(8, 8)
-	ffmt.Puts(pb)
+	NewPiecesBoard(8, 8)
+}
+
+func TestHoldPiece(t *testing.T) {
+	pb2 := NewPiecesBoard(4, 4)
+	p1 := NewPiece(2, 2)
+	pb2.HoldPiece(&p1)
+	ffmt.Puts(pb2.Board)
+	pb2.ClearUse()
+	ffmt.Puts(pb2.Board)
 }
